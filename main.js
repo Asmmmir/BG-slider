5301cb83d59ffd9012118b3409bcf941416fe4c7
Aslanbek Astamirov <asmmmmir@gmail.com>
2023-02-20 18:09:25 +0500

    new

---
+++ main.js
@@ -0,0 +1,44 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+
+  setBgToBody();
+  setActiveSlide();
+});
+
+setBgToBody();
+
+function setBgToBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  slides[activeSlide].classList.add("active");
+}
+
+// url('https://mobimg.b-cdn.net/v3/fetch/94/94c56e15f13f1de4740a76742b0b594f.jpeg');
